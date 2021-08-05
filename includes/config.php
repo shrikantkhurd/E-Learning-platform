4d8854a35bce19dd5fd6a4dd496beec0b6dcdae4
shrikantkhurd <63811430+shrikantkhurd@users.noreply.github.com>
2021-08-06 00:29:23 +0530

    initial commit

---
+++ includes/config.php
@@ -0,0 +1,14 @@
+<?php
+$servername= "localhost";
+$username= "root";
+$password= "";
+$database= "learner";
+$con = mysqli_connect($servername, $username, $password,$database);
+
+
+// Check connection
+if (mysqli_connect_errno())
+{
+ echo "Failed to connect to MySQL: " . mysqli_connect_error();
+}
+?>
